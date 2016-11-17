19298a6e9b82137ce03afe4a180a0197cd02d63e
andrewlaser <andrew_laser@hotmail.com>
2016-11-17 22:37:12 +0200

    Homework for Lecture2 (Infrastructure)

---
+++ Homework2/Homework2/Infrastructure.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Homework2
+{
+    [TestFixture]
+    public class Infrastructure
+    {
+        private IWebDriver _driver;
+        private WebDriverWait _wait;
+
+        [SetUp]
+        public void Start()
+        {
+            _driver = new ChromeDriver();
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(100));
+        }
+
+
+        [Test]
+        public void TestMethod1()
+        {
+            _driver.Url = @"http://www.bbc.com/";
+            _driver.FindElement(By.Id("orb-search-q")).SendKeys("Weather");
+            _driver.FindElement(By.XPath("//button[@class='se-searchbox__submit']")).Click();
+            _wait.Until(ExpectedConditions.ElementExists(By.XPath("//a[contains(text(), 'BBC Weather')]")));
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            _driver.Quit();
+            _driver = null;
+
+        }
+    }
+}
