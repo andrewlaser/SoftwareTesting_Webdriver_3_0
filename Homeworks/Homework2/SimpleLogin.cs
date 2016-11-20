7031b80044f71af3015614ec552bc3e1a91938d4
andrewlaser <andrew_laser@hotmail.com>
2016-11-20 15:32:50 +0200

    Homework2 - Litecart admin login scenario
    From not WebDriver_3_0_Homeworks.sln - solution for all homeworks.
    Homework2 folder - for Homework N2
    SimpleLogin.cs - Simple login scenario for Litecart admin

---
+++ Homeworks/Homework2/SimpleLogin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Homework2
+{
+    [TestFixture]
+    public class SimpleLogin
+    {
+        private IWebDriver _driver;
+        private WebDriverWait _wait;
+        //private const string _hostName = "http://192.168.1.151";
+        private const string _hostName = "http://localhost";
+
+        [SetUp]
+        public void Start()
+        {
+            _driver = new ChromeDriver();
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(100));
+        }
+
+
+        [Test]
+        public void Login()
+        {
+            _driver.Url = _hostName + @"/litecart/admin/";
+            _driver.FindElement(By.XPath("//input[@name='username']")).SendKeys("admin");
+            _driver.FindElement(By.XPath("//input[@name='password']")).SendKeys("admin");
+            _driver.FindElement(By.XPath("//button[@name='login']")).Click();
+            _wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//a[@title='Logout']")));
+
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            _driver.Quit();
+            _driver = null;
+
+        }
+    }
+}
