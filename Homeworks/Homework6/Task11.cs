5787ff8057df94ce90c61f1eb92108599fee4077
andrewlaser <andrew_laser@hotmail.com>
2016-12-11 18:00:22 +0200

    Homework6- task11 and started Task12

---
+++ Homeworks/Homework6/Task11.cs
@@ -0,0 +1,80 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Homework6
+{
+    [TestFixture]
+    public class Task11
+    {
+        private IWebDriver _driver;
+        private WebDriverWait _wait;
+        private const string _hostName = "http://192.168.1.151";
+        //private const string _hostName = "http://localhost";
+
+        [SetUp]
+        public void Start()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(30));
+        }
+
+
+        [Test, Description("Задание 11. Сделайте сценарий регистрации пользователя")]
+        public void RegisterUserInShop()
+        {
+
+            string timestampStr = DateTime.Now.ToString("yyyyMMddHHmmssffff");
+            string emailStr = $"{timestampStr}mail@test.com";
+            string passwordStr = "Password123";
+
+            //Navigate to registration form
+            _driver.Url = _hostName + "/litecart/en/";
+            _driver.FindElement(By.XPath("//form[@name='login_form']//a[contains(text(),'New customers click here')]")).Click();
+
+            //Fill registration form
+            _driver.FindElement(By.Name("firstname")).SendKeys("Fname");
+            _driver.FindElement(By.Name("lastname")).SendKeys("Lname");
+            _driver.FindElement(By.Name("address1")).SendKeys("Address 1");
+            _driver.FindElement(By.Name("postcode")).SendKeys("12345");
+            _driver.FindElement(By.Name("city")).SendKeys("City");
+            var countrySelect = new SelectElement(_driver.FindElement(By.Name("country_code")));
+            countrySelect.SelectByValue("UA");
+            _driver.FindElement(By.Name("email")).SendKeys(emailStr);
+            _driver.FindElement(By.Name("phone")).SendKeys("+380671234567");
+            _driver.FindElement(By.Name("password")).SendKeys(passwordStr);
+            _driver.FindElement(By.Name("confirmed_password")).SendKeys(passwordStr);
+
+            //submit registration form
+            _driver.FindElement(By.Name("create_account")).Click();
+
+            //logout
+            _driver.FindElement(By.XPath("//div[@id='box-account']//a[contains(text(),'Logout')]")).Click();
+
+            //login
+            _driver.FindElement(By.XPath("//input[@name='email']")).SendKeys(emailStr);
+            _driver.FindElement(By.XPath("//input[@name='password']")).SendKeys(passwordStr);
+            _driver.FindElement(By.XPath("//button[@name='login']")).Click();
+
+            //logout again
+            _driver.FindElement(By.XPath("//div[@id='box-account']//a[contains(text(),'Logout')]")).Click();
+        }
+
+
+        [TearDown]
+        public void Stop()
+        {
+            _driver.Quit();
+            _driver = null;
+
+        }
+    }
+}
---
+++ Homeworks/Homework6/Task12.cs
@@ -0,0 +1,93 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Homework6
+{
+    [TestFixture]
+    public class Task12
+    {
+        private IWebDriver _driver;
+        private WebDriverWait _wait;
+        private const string _hostName = "http://192.168.1.151";
+        //private const string _hostName = "http://localhost";
+
+        [SetUp]
+        public void Start()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(30));
+        }
+
+
+        [Test, Description("Задание 12. Сделайте сценарий добавления товара")]
+        public void AddProductInAdmin()
+        {
+            //constants
+            string timestampStr = DateTime.Now.ToString("yyyyMMddHHmmssffff");
+            string productNameStr = "product " + timestampStr;
+            string selectCategoryStr = "Rubber Ducks";
+
+            //Login and Navigate to http://localhost/litecart/admin/?app=countries&doc=countries
+            _driver.Url = _hostName + @"/litecart/admin/";
+            _driver.FindElement(By.XPath("//input[@name='username']")).SendKeys("admin");
+            _driver.FindElement(By.XPath("//input[@name='password']")).SendKeys("admin");
+            _driver.FindElement(By.XPath("//button[@name='login']")).Click();
+            _wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//a[@title='Logout']")));
+
+            //Navigate to Add new product form
+            _driver.FindElement(By.XPath("//a[./span[contains(text(),'Catalog')]]")).Click();
+            _driver.FindElement(By.XPath("//a[contains(text(),'Add New Product')]")).Click();
+
+            //Fill in Add New Product form
+            _driver.FindElement(By.XPath("//input[@name='status' and @isChecked='1']")).Click();
+            _driver.FindElement(By.XPath("//input[contains(@name, 'name[')]")).SendKeys(timestampStr);
+            SelectCategories(new List<string>() {"selectCategory"});
+            SelectElement defaultCategoryElement = new SelectElement(_driver.FindElement(By.Name("default_category_id")));
+            defaultCategoryElement.SelectByText(selectCategoryStr);
+            //TODO finish
+
+
+
+
+
+        }
+
+        /// <summary>
+        /// Select categories from the list
+        /// </summary>
+        /// <param name="categoryNames"></param>
+        /// <param name="isChecked"></param>
+        public void SelectCategories(List<string> categoryNames)
+        {
+            foreach (var categoryName in categoryNames)
+            {
+                var checkbox =
+                    _driver.FindElement(By.XPath($"//input[@name='categories[]' and @data-name='{categoryName}']"));
+                SetCheckboxValue(checkbox, true);
+            }
+        }
+
+        public void SetCheckboxValue(IWebElement element, bool isChecked)
+        {
+            if(element.Selected != isChecked)
+                element.Click();
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            _driver.Quit();
+            _driver = null;
+
+        }
+    }
+}
