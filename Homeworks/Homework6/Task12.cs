4b118537d5d64a4832cdbaa0cefc28f0920e82a1
andrewlaser <andrew_laser@hotmail.com>
2016-12-11 23:07:14 +0200

    Updates for task12

--- Homeworks/Homework6/Task12.cs
+++ Homeworks/Homework6/Task12.cs
@@ -53,12 +53,56 @@ namespace Homework6
             SelectCategories(new List<string>() {selectCategoryStr});
             SelectElement defaultCategoryElement = new SelectElement(_driver.FindElement(By.Name("default_category_id")));
             defaultCategoryElement.SelectByText(selectCategoryStr);
+            CheckProductGroups(new List<string>() {"Male"});
+            _driver.FindElement(By.Name("quantity")).SendKeys("5");
+            new SelectElement(_driver.FindElement(By.Name("quantity_unit_id"))).SelectByText("pcs");
+            new SelectElement(_driver.FindElement(By.Name("delivery_status_id"))).SelectByText("3-5 days");
+            new SelectElement(_driver.FindElement(By.Name("sold_out_status_id"))).SelectByText("Sold out");
+            AttachFile(_driver.FindElement(By.Name("new_images[]")), @"D:\Trainings\WebDriver3_0\GitHub\SoftwareTesting_Webdriver_3_0\Homeworks\Homework5\Task9.cs");
+            _driver.FindElement(By.Name("date_valid_from")).SendKeys("2016-12-10");
+            _driver.FindElement(By.Name("date_valid_to")).SendKeys("2026-12-10");
+
+            _driver.FindElement(By.XPath("//button[@value='Save']")).Click();
+
             //TODO finish
 
 
 
 
 
+        }
+
+        public void AttachFile(IWebElement element, string path)
+        {
+            Unhide(element);
+            element.SendKeys(path);
+        }
+        
+        /// <summary>
+        /// Unhide invisible element
+        /// e.g. to attach a file to invisible field
+        /// </summary>
+        /// <param name="driver"></param>
+        /// <param name="element"></param>
+        public void Unhide(IWebElement element)
+        {
+            String script = @"arguments[0].style.opacity=1;"
+              + "arguments[0].style['transform']='translate(0px, 0px) scale(1)';"
+              + "arguments[0].style['MozTransform']='translate(0px, 0px) scale(1)';"
+              + "arguments[0].style['WebkitTransform']='translate(0px, 0px) scale(1)';"
+              + "arguments[0].style['msTransform']='translate(0px, 0px) scale(1)';"
+              + "arguments[0].style['OTransform']='translate(0px, 0px) scale(1)';"
+              + "return true;";
+            (_driver as IJavaScriptExecutor).ExecuteScript(script, element);
+        }
+
+        public void CheckProductGroups(List<string> productGroups)
+        {
+            foreach (var productgroup in productGroups)
+            {
+                IWebElement element =_driver.FindElement(By.XPath($"//tr[./td[contains(text(),'{productgroup}')]]//input[@type='checkbox']"));
+                SetCheckboxValue(element, true);
+            }
         }
 
         /// <summary>
